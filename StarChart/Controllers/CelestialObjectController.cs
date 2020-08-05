42f2025afbad4a27cfa49b5259c0ec5c26f151d9
Hunter Esposito <hesposito@tutanota.com>
2020-08-05 08:02:26 -0400

    Fixed CelestialObjectController's base class

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -11,7 +11,7 @@ namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class CelestialObjectController : Controller
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 

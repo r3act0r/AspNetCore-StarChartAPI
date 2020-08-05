da85ac7ba170f96420bc1d4444c837350137b46a
Hunter Esposito <hesposito@tutanota.com>
2020-08-05 07:53:46 -0400

    Added 'AddMvc()' to ConfigureServices

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -12,6 +12,8 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseInMemoryDatabase("StarChart"));
         }
